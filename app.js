4553c0c6fc050d85dec43813d3ef4a7eee172d12
Mario Roberto Molano <mariormolano@yahoo.com>
2024-10-03 10:47:59 -0500

    se agrega express

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // main.js
-
+import express from "express";
 import x from "sqlite3";
 
 const sqlite3 = x.verbose();
+
+const app = express();
+const port = 3000;
 
 // Connect to a database (in this example, a new file-based database)
 const db = new sqlite3.Database("mydatabase.db");
@@ -31,4 +34,12 @@ db.close((err) => {
     return console.error(err.message);
   }
   console.log("Database connection closed");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
